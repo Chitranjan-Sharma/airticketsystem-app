3cd71e2f400bed3f438a282e670e76b8c2061eb2
chitranjan <chitranjank.co@gmail.com>
2022-12-09 01:44:28 +0530

    UI implementation completed

--- src/app/admin/feedbacks/feedbacks.component.ts
+++ src/app/admin/feedbacks/feedbacks.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { API } from 'src/app/api/api.service';
+import { Feedback } from 'src/app/models/feedback.model';
 
 @Component({
   selector: 'app-feedbacks',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./feedbacks.component.css']
 })
 export class FeedbacksComponent {
+  constructor(public api: API) {
+  }
 
+  ngOnInit(): void {
+    this.fetchAllFeedbacks();
+  }
+
+  feedbacks: Feedback[] = [];
+  allFeedbacks: Feedback[] = [];
+
+  searchByData: string = '';
+
+  searchFeedbacks() {
+    this.feedbacks = [];
+    this.allFeedbacks.forEach(element => {
+      if (element.FullName.toLowerCase().includes(this.searchByData.toLowerCase()) ||
+        element.Email.toLowerCase().includes(this.searchByData.toLowerCase()) ||
+        element.CustomerId == parseInt(this.searchByData)) {
+
+        this.feedbacks.push(element);
+
+      }
+    });
+
+  }
+
+  viewFeedback(feedback: Feedback) {
+
+  }
+  fetchAllFeedbacks() {
+    this.api.getFeedbacks().subscribe((res) => {
+      this.allFeedbacks = res as Feedback[];
+      this.feedbacks = this.allFeedbacks;
+    })
+  }
 }
