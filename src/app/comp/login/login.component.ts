d50de562f598c0d0cbbf84d71bf2fca29a079e23
chitranjan <chitranjank.co@gmail.com>
2022-12-10 16:36:33 +0530

    Forgot Password ui added

--- src/app/comp/login/login.component.ts
+++ src/app/comp/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { API } from 'src/app/api/api.service';
+import { Register } from 'src/app/models/register.model';
 
 @Component({
   selector: 'app-login',
@@ -17,6 +18,7 @@ export class LoginComponent implements OnInit {
   email: string = '';
   password: string = '';
   userName: string = '';
+
 
   adminLogin() {
     this.router.navigate(["comp/admin"]);
@@ -71,5 +73,45 @@ export class LoginComponent implements OnInit {
       alert("Password not matched");
     }
 
+  }
+
+  searchingEmail: string = '';
+  actionVb: string = "Search";
+
+  isUserFound: boolean = false;
+  searchedUserData: Register = new Register;
+  newPassword: string = '';
+
+  searchAccount() {
+    this.actionVb = "Searching...";
+    this.api.getUserData().subscribe((res) => {
+      let users: Register[] = [];
+      users = res as Register[];
+
+      if (this.searchingEmail != '' && users != null) {
+        users.forEach(element => {
+          if (element.Email.toLowerCase().includes(this.searchingEmail.toLowerCase())) {
+            this.isUserFound = true;
+            this.searchedUserData = element;
+            this.actionVb = "Found";
+          }
+        });
+      }
+    })
+  }
+
+  actionVbReset: string = "Reset";
+  resetPassword() {
+    this.searchedUserData.Password = this.newPassword;
+    if (this.newPassword != '' && this.newPassword.length >= 6 && this.searchedUserData.CustomerId != 0) {
+      this.api.putUserData(this.searchedUserData).subscribe((res) => {
+        this.actionVbReset = "Done";
+        this.searchedUserData = new Register;
+        this.searchingEmail = "";
+        this.newPassword = "";
+      }, (error) => {
+        console.log(error);
+      })
+    }
   }
 }
